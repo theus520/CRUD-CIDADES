61686934803dfb05125f908d48139d55e1531852
Matheus rabelo <matheusrabelo520@gmail.com>
2023-10-07 11:49:34 -0300

    Criando a página html do projeto

--- src/main/java/br/rabelo/especializacao/crudcidades/view/CidadeController.java
+++ src/main/java/br/rabelo/especializacao/crudcidades/view/CidadeController.java
@@ -1,10 +1,12 @@
 package br.rabelo.especializacao.crudcidades.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CidadeController {
 
+	@GetMapping("/")
 	public String listarCidades() {
 		return "crud.html";
 	}
