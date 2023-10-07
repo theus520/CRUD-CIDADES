f6ef870b10e8b2d33767a47c873234dca7ca6af4
Matheus rabelo <matheusrabelo520@gmail.com>
2023-10-07 11:29:01 -0300

    Criando o projeto

---
+++ src/main/java/br/rabelo/especializacao/crudcidades/ProjectCrudCidadesApplication.java
@@ -0,0 +1,13 @@
+package br.rabelo.especializacao.crudcidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCrudCidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCrudCidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/rabelo/especializacao/crudcidades/ProjectCrudCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rabelo.especializacao.crudcidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCrudCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
